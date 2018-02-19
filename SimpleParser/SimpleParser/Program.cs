50d44675d9efb38e8db6c3c1c03780724ce703b1
Jacob Martin <jacobmartin@me.com>
2018-02-18 22:19:56 -0600

    First commit

---
+++ SimpleParser/SimpleParser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This will be a simple parser");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
